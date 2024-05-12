6f66c885cea2b5abdbfc7dcfcc9b78c469283b65
Milind Mishra <milind.mishra4@gmail.com>
2024-05-12 17:26:36 +0530

    📦 NEW: animate bg text andd scale with a range of values in an array

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,11 @@ const Page = () => {
     <div className="flex items-center justify-center min-h-screen">
       <motion.button
         className="bg-black text-white px-3 py-2 rounded-md"
-        animate={{ scale: 2 }}
+        animate={{
+          backgroundColor: ["#ff0000", "#00ff00", "#0000ff"],
+          color: ["#0000ff", "#ff0000", "#00ff00"],
+          transform: ["scale(1)", "scale(1.5)", "scale(1)"],
+        }}
       >
         Scaled!
       </motion.button>
